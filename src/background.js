d3dff541e1f5e2ede4adb95562d4507d09480532
Jakub Jirák (CS) <Jakub.Jirak@gendigital.com>
2023-03-15 12:44:31 +0100

    Remove feedback form trigger

--- src/background.js
+++ src/background.js
@@ -45,17 +45,6 @@ chrome.tabs.onUpdated.addListener(
   }
 );
 
-// chrome.runtime.onInstalled.addListener((details) => {
-//   if (['install', 'update'].includes(details.reason)) {
-//     const feedbackFormId =
-//       '1FAIpQLSdItN10f-8zD6amnFu-WfjTB8rq_ACeHW3r-WRse0N620-UNQ';
-//     const feedbackFormUrl = `https://docs.google.com/forms/d/e/${feedbackFormId}/viewform`;
-//     if (chrome.runtime.setUninstallURL) {
-//       chrome.runtime.setUninstallURL(feedbackFormUrl);
-//     }
-//   }
-// });
-
 function getTotals(url, payload) {
   let finalUrl = `${API_URL}${url}?limit=50`;
   if (!payload) {
